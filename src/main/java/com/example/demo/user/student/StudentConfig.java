1f95925c7108c9ebe33e82b8562ba1eb2446b636
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2024-12-16 20:45:37 +0200

    some changes

--- src/main/java/com/example/demo/user/student/StudentConfig.java
+++ src/main/java/com/example/demo/user/student/StudentConfig.java
@@ -1,26 +1,31 @@
 package com.example.demo.user.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student omar = new Student(1L,
+            Student omar = new Student(
                     "Omar",
                     "omar@gmail.com"
                     ,"123");
 
-            Student khaled = new Student(1L,
+            Student khaled = new Student(
                     "khaled",
                     "khaled@gmail.com"
                     ,"1234");
 
-            repository.s
+            repository.saveAll(
+                    List.of(omar, khaled)
+            );
         };
     }
 }
--- src/main/java/com/example/demo/user/student/StudentService.java
+++ src/main/java/com/example/demo/user/student/StudentService.java
@@ -1,18 +1,20 @@
 package com.example.demo.user.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L,
-                        "Omar",
-                        "omar@gmail.com"
-                        ,"123")
-        );
+        return studentRepository.findAll();
     }
 }
