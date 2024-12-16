c5bbd3992a1fee4d0d1238120e0ffb509967c482
Omar Ahmed <138378887+Omar-Badwilan@users.noreply.github.com>
2024-12-16 14:25:26 +0200

    api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,30 +1,18 @@
 package com.example.demo;
-import com.example.demo.user.Student;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	//returns json object
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(1L,
-						"Omar",
-						"omar@gmail.com"
-						,"123")
-		);
-	}
+
 
 }
