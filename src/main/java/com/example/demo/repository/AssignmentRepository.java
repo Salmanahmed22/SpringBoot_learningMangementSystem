8869fad454f9c4371b2c02a2bad7e95d5e5391eb
Omar Ahmed <138378887+Omar-Badwilan@users.noreply.github.com>
2024-12-19 18:21:33 +0200

    added repositry annotation

--- src/main/java/com/example/demo/repository/AssignmentRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.models.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 }
