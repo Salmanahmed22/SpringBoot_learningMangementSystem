6194de2aeab0bdd1f41a611821da8303578844e8
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2024-12-21 20:44:33 +0200

    delete extra createCOurse

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -35,15 +35,6 @@ public class CourseController {
         return ResponseEntity.ok(courseService.getAllCourses());
     }
 
-    @PostMapping
-    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
-        try {
-            return ResponseEntity.ok(courseService.createCourse(course));
-        }
-        catch (IllegalArgumentException ex) {
-            return ResponseEntity.badRequest().body(null);
-        }
-    }
 
     @PutMapping("/{id}")
     public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course updatedCourse) {
