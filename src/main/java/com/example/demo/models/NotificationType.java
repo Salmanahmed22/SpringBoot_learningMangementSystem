654cfce54cf8e571901d6c593ac969a4b6834142
Omar Ahmed <138378887+Omar-Badwilan@users.noreply.github.com>
2024-12-23 03:17:47 +0200

    websocket + Notification

---
+++ src/main/java/com/example/demo/models/NotificationType.java
@@ -0,0 +1,10 @@
+package com.example.demo.models;
+
+public enum NotificationType {
+    ENROLLMENT_CONFIRMATION,
+    ASSIGNMENT_GRADED,
+    QUIZ_RESULTS,
+    COURSE_UPDATE,
+    SYSTEM_ALERT,
+    ATTENDANCE_REMINDER
+}
